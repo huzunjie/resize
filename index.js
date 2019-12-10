fd3ac141d936da129ab652f87f78edb9542272e8
胡尊杰 <feixiang_hu@qq.com>
2019-12-10 19:37:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,27 +20,35 @@ const dirConf = [
   {
     posKey: 'left',
     sizeKey: 'width',
-    pageKey: 'pageX'
+    pageKey: 'pageX',
+    cursor: 'ew-resize'
   },
   {
     posKey: 'top',
     sizeKey: 'height',
-    pageKey: 'pageY'
+    pageKey: 'pageY',
+    cursor: 'ns-resize'
   }
 ];
+const dirArea2Name = [
+  ['left', 'right'],
+  ['top', 'bottom']
+];
+const defConf = {
+  dir: 0, // 拖拽方向：水平 - 0 ||  垂直 - 1 
+  area: 1, // 可拖拽位置：最小边 - 0 ||  最大边 - 1 （横向：左边为最小边、右边是最大边；纵向：按上下两边划分）
+  min: 150,
+  max: 600
+};
 
 // 拖拽控制器
 const ctrl = {
   el: null, // 拖拽时要改动的元素
-  dir: 0, // 拖拽方向：水平 - 0 ||  垂直 - 1 
-  area: 1, // 可拖拽位置：最小边 - 0 ||  最大边 - 1 （横向：左边为最小边、右边是最大边；纵向：按上下两边划分）
-  min: 150,
-  max: 600,
+  ...defConf,
   setOpts(opts) {
-    if(!isNaN(opts.min)) ctrl.min = opts.min;
-    if(!isNaN(opts.max)) ctrl.max = opts.max;
-    if(!isNaN(opts.dir)) ctrl.dir = opts.dir;
-    if(!isNaN(opts.area)) ctrl.area = opts.area;
+    Object.keys(defConf).forEach(_key => {
+      if(!isNaN(opts[_key])) ctrl[_key] = opts[_key];
+    });
   },
   // 获取边界距离值
   getHorizCriticalVal(evt) {
@@ -63,7 +71,7 @@ const ctrl = {
   enterResizeBar(evt, opts) {
     opts && ctrl.setOpts(opts);
     ctrl.el = evt.currentTarget;
-    setDocElCursor('ew-resize');
+    setDocElCursor(dirConf[ctrl.dir].cursor);
   },
   // 鼠标离开拖拽区
   leaveResizeBar() {
@@ -78,7 +86,8 @@ const ctrl = {
     const el = ctrl.el;
     if(!el) return;
     const dirKeys = dirConf[ctrl.dir];
-    ctrl.staElSize = el.getBoundingClientRect()[dirKeys.sizeKey] - evt[dirKeys.pageKey];
+    ctrl.staElSize = el.getBoundingClientRect()[dirKeys.sizeKey];
+    ctrl.staPagePos = evt[dirKeys.pageKey];
     addDocElEvt('mousemove', ctrl.dragging);
     addDocElEvt('mouseup', ctrl.dragEnd);
     el.setCapture && el.setCapture();
@@ -92,7 +101,9 @@ const ctrl = {
     }
     const dirKeys = dirConf[ctrl.dir];
     evt.preventDefault();
-    el.style[dirKeys.sizeKey] = Math.max(ctrl.min, Math.min(ctrl.max, ctrl.staElSize + evt[dirKeys.pageKey])) + 'px';
+    
+    const posDiff = (evt[dirKeys.pageKey] - ctrl.staPagePos) * (ctrl.area === 1 ? 1 : -1);
+    el.style[dirKeys.sizeKey] = Math.max(ctrl.min, Math.min(ctrl.max, ctrl.staElSize + posDiff)) + 'px';
   },
   // 改变尺寸
   dragEnd(evt) {
@@ -100,10 +111,32 @@ const ctrl = {
     removeDocElEvt('mousemove', ctrl.dragging);
     removeDocElEvt('mouseup', ctrl.dragEnd);
     ctrl.staElSize = null;
+    ctrl.staPagePos = null;
     el && el.releaseCapture && el.releaseCapture();
   },
   init() {
     addDocElEvt('mousedown', ctrl.dragStart);
+  },
+  // 给 VUE 组件的 props 增加拖拽能力
+  dragResize2Vue(props, conf = {}) {
+    const resizeOpts = Object.assign({}, defConf, conf);
+    const {dir = 0, area = 1} = resizeOpts;
+    if(typeof props.class === 'string') {
+      props.class = {
+        [props.class]: true
+      };
+    }
+    // 增加若拽标识 className
+    props.class = {
+      ...props.class,
+      ['drag-resize-' + dirArea2Name[dir][area]]: true
+    };
+    // 侧边增加鼠标事件，判定拖拽
+    props.on = {
+      ...props.on,
+      mousemove: evt => ctrl.hoverResizeBar(evt, resizeOpts),
+      mouseleave: evt => ctrl.leaveResizeBar(evt)
+    };
   }
 };
 ctrl.init();
